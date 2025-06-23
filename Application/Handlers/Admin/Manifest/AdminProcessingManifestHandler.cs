0b3d0bb6ef6b64c7eb7ca1584a38dd292a0a3d5a
morteza <mortezasojodi70@gmail.com>
2025-06-23 13:00:19 +0330

    set in process

--- Application/Handlers/Admin/Manifest/AdminProcessingManifestHandler.cs
+++ Application/Handlers/Admin/Manifest/AdminProcessingManifestHandler.cs
@@ -7,19 +7,24 @@ using CMPNatural.Core.Entities;
 using ScoutDirect.Core.Entities.Base;
 using System.Linq;
 using CMPNatural.Core.Enums;
+using Microsoft.EntityFrameworkCore;
 
 namespace CMPNatural.Application
 {
     public class AdminProcessingManifestHandler : IRequestHandler<AdminProcessingManifestCommand, CommandResponse<Manifest>>
     {
         private readonly IManifestRepository _repository;
-        public AdminProcessingManifestHandler(IManifestRepository _repository)
+        private readonly IinvoiceRepository _iinvoiceRepository;
+        public AdminProcessingManifestHandler(IManifestRepository _repository, IinvoiceRepository _iinvoiceRepository)
         {
             this._repository = _repository;
+            this._iinvoiceRepository = _iinvoiceRepository;
         }
         public async Task<CommandResponse<Manifest>> Handle(AdminProcessingManifestCommand request, CancellationToken cancellationToken)
         {
-            var result = (await _repository.GetAsync(x=>x.Id == request.Id && x.Status == ManifestStatus.Scaduled)).FirstOrDefault();
+            var result = (await _repository.GetAsync(x=>x.Id == request.Id && x.Status == ManifestStatus.Scaduled,
+                query=> query.Include(x=>x.Invoice).ThenInclude(x=>x.BaseServiceAppointment)
+                )).FirstOrDefault();
 
             if (result == null)
             {
@@ -27,6 +32,14 @@ namespace CMPNatural.Application
                 {
                     Message = "This manifest has already been assigned to a provider and cannot be reassigned."
                 };
+            }
+
+            //var invoices = await _iinvoiceRepository.GetAsync(x=>x.InvoiceId == result.Invoice.InvoiceId && (x.Status == InvoiceStatus.Processing_Provider));
+            result.Status = ManifestStatus.Assigned;
+            result.Invoice.Status = InvoiceStatus.Processing_Provider;
+            foreach (var item in result.Invoice.BaseServiceAppointment)
+            {
+                item.Status = ServiceStatus.Proccessing;
             }
 
 
