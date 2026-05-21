15079ad0bcf93b31ef3062e12a5843872dc82b9a
morteza <mortezasojodi70@gmail.com>
2026-05-21 14:45:56 +0330

    .

--- Core/Entities/WhiteLabelRequest.cs
+++ Core/Entities/WhiteLabelRequest.cs
@@ -1,9 +1,23 @@
 ﻿using System;
+
 namespace CMPNatural.Core.Entities
 {
-	public class WhiteLabelRequest
-	{
-		public object MyProperty { get; set; }
-	}
-}
+    public class WhiteLabelRequest
+    {
+        public bool ManageClientsDirectly { get; set; }
 
+        public bool WantsClientPortal { get; set; }
+
+        public bool WantsDispatchManagement { get; set; }
+
+        public bool WantsInvoicing { get; set; }
+
+        public bool WantsWhiteLabelBranding { get; set; }
+
+        public bool WantsCustomDomain { get; set; }
+
+        public string? CustomDomain { get; set; }
+
+        public string? SubDomain { get; set; }
+    }
+}
